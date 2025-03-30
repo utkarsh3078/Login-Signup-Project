db2023a7c5522f013719aaba088e14a82b449d57
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2025-03-30 15:46:38 +0530

    Fourth commit
    Signup page successfully created

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,8 +1,58 @@
-import React from "react"
-import { link } from "react-router-dom"
+import React, {useState} from "react"
+import { link, useNavigate } from "react-router-dom"
 import {ToastContainer} from "react-toastify"
+import { handleError, handleSuccess } from "../utils"
 
 function Signup(){
+
+    const [signupInfo, setSignupInfo] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const navigate = useNavigate();
+    const handleChange = (e)=>{
+        const {name, value} = e.target;
+        console.log(name,value);
+        const copySignupInfo = {...signupInfo};
+        copySignupInfo[name] = value;
+        setSignupInfo(copySignupInfo);
+    }
+
+    const handleSignup = async(e)=>{
+        e.preventDefault();
+        const {name, email, password} = signupInfo;
+        if(!name || !email || !password){
+            return handleError("Please fill all the fields");
+        } 
+        try{
+            const url = "http://localhost:8080/auth/signup"; 
+            const response = await fetch(url,{
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(signupInfo)
+            });
+            const result = await response.json();
+            console.log(result);
+            const { success, message, error } = result;
+            if(success){
+                handleSuccess(message);
+                setTimeout(()=>{
+                    navigate("/login")
+                }, 1000)
+            }else if(error){
+                const details = error?.details[0].message;
+                handleError(details);
+            }else if(!success){
+                handleError(message);
+            }
+        }catch(err){
+            handleError(err);
+        }
+    }
     return(
         <div class="container">
             <div class="logo">
@@ -10,32 +60,38 @@ function Signup(){
             </div>
             <div class="form">
             <h1>Signup</h1>
-            <form>
+            <form onSubmit={handleSignup}>
                 <div>
                     <label htmlFor="name">Name</label>
                     <input 
+                        onChange={handleChange}
                         type="text"
                         name="name"
                         autoFocus
                         placeholder="Enter your name"
+                        value={signupInfo.name}
                     />
                 </div>
 
                 <div>
                     <label htmlFor="email">Email</label>
                     <input 
+                        onChange={handleChange}
                         type="email"
                         name="email"
                         placeholder="Enter your Email"
+                        value={signupInfo.email}
                     />
                 </div>
 
                 <div>
                     <label htmlFor="password">Password</label>
                     <input 
+                        onChange={handleChange}
                         type="password"
                         name="password"
                         placeholder="Enter your password"
+                        value={signupInfo.password}
                     />
                 </div>
                 <button>Signup</button>
