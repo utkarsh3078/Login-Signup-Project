da7528386b9e0aa7c6228554270ab776d5824c88
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2025-03-30 17:59:07 +0530

    Final Commit
    Added private routing and project completed successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,23 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
+import { useState } from 'react';
+import RefreshHandler from './RefreshHandler';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element })=>{
+    return isAuthenticated ? element : <Navigate to="/login"></Navigate>
+  }
   return (
     <div className="App">
+      <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
       <Route path="/" element={<Navigate to={"/login"}></Navigate>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<PrivateRoute element={<Home />} />} />
       </Routes>
     </div>
   );
